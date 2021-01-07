0d86d1787792b51a80c5883f3c4bd2faf725054c
bradenslsb <bbir1199@gmail.com>
2021-01-07 09:38:13 -0700

    Added the environs libary

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow 
 from flask_cors import CORS 
 from flask_heroku import Heroku 
+from environs import Env
 import os
 
 app = Flask(__name__)
